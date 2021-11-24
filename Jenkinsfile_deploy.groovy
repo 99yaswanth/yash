b37e5709d65f1eea3ec34b0deb0a5ae941e7c4c2
99yaswanth <yashwanth24k@gmail.com>
2021-11-24 08:10:40 +0530

    added build

---
+++ Jenkinsfile_deploy.groovy
@@ -0,0 +1,27 @@
+//Declerative pipeline
+pipeline{
+    agent any
+    parameters{
+        string(name: 'BRANCH_NAME', defaultValue: 'master', description: 'give branch name')
+        string(name: 'SERVER_IP', defaultValue: '', description: 'give IP address')
+        string(name: 'BUILD_NUMBER', defaultValue: '', description: 'give build num')
+    }
+    stages{
+        stage("download artifacts"){
+            steps{
+                println "downloading artifacts from s3"
+                sh """
+                aws s3 ls s3://yashwanth12
+                aws s3 cp s3://yashwanth12/hello-${BUILD_NUMBER}.war .
+                """
+            }
+        }
+        stage("copy artifact"){
+            steps{
+                println "copying artifacts "
+                sh "ssh -i /tmp/nvirginia.pem ec2-user@${SERVER_IP} "\systemctl status tomcat\""
+                sh "scp -i /tmp/nvirginia.pem hello-${BUILD_NUMBER}.war ec2-user@${SERVER_IP}:/tmp/"
+            }
+        }
+    }
+}
